a9f27fd654f700ff8582c156a8d9d399bfa9a3e7
Chaithra Jupudi <chaithra.jupudi@gmail.com>
2022-03-14 18:51:44 +0530

    Praciticed Variables in JS

---
+++ Variables.js
@@ -0,0 +1,21 @@
+let x = "Hello"
+let y = 3
+let z = 3.4
+
+console.log(x, y, z)
+
+
+let message;
+
+message = 'Hello!';
+
+message = 'World!';
+
+//alert(message);
+
+let hello = 'Hello world!';
+
+message = hello;
+
+alert(hello);
+alert(message);
