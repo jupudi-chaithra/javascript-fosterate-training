9b0d69b39ccdc9ab9fa77d7e97f915998652fa53
Chaithra Jupudi <chaithra.jupudi@gmail.com>
2022-03-16 07:39:38 +0530

    Practiced operators

---
+++ Operators.js
@@ -0,0 +1,22 @@
+console.log(5 + 2);
+console.log(5 - 2);
+console.log(5 * 2);
+console.log(5 / 2);
+console.log(5 / 2);
+console.log(5 % 2);
+console.log(5 ** 2);
+
+console.log('1' + 2);
+console.log(2 + 2 + '1');
+console.log('1' + 2 + 2);
+
+
+let counter = 2;
+counter++;
+console.log( counter );
+
+++counter
+console.log(counter);
+
+let a = (1 + 2, 3 + 4);
+console.log(a);
