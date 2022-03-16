fb037e8267bbf22d824bfd11d84b8cfc88eac93b
Chaithra Jupudi <chaithra.jupudi@gmail.com>
2022-03-16 07:05:25 +0530

    Practiced datatypes

---
+++ Datatypes.js
@@ -0,0 +1,29 @@
+/*
+alert(1 / 0)
+alert("hi" / 2)
+
+const bigInt = 1234567890123456789012345678901234567890
+alert(bigInt)
+
+bigInt = 1234567890123456789012345678901234567890n
+alert(bigInt)*/
+
+/*str1 = "Hello"
+str2 = 'World'
+str3 = `The result is ${str1 + ' ' + str2}`
+
+console.log(str1)
+console.log(str2)
+console.log(str3)*/
+
+/*let bool = true
+console.log(bool);*/
+
+console.log(typeof(0));
+console.log(typeof(1.5));
+console.log(typeof("c"));
+console.log(typeof("chaithra"));
+console.log(typeof(true));
+console.log(typeof(undefined));
+console.log(typeof(null));
+console.log(typeof(alert));
